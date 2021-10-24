3fd6b17869fc99f1db76d7afd384e7b02d5cfee5
Bong <poseson92@naver.com>
2021-10-25 03:42:08 +0900

    Update5

--- server.js
+++ server.js
@@ -12,17 +12,9 @@ dotenv.config({ path: "./config.env" });
 app.use(express.json({ extended: false }));
 app.use("/api/register", require("./routes/api/register"));
 
-app.get("/", cors(), (req, res) => {
-  res.send("cors!");
-});
-
-app.post("/", cors(), (req, res) => {
-  res.send("cors!!");
-});
-
-// get요청시 "API Running" 을 response 해주기
 app.get("/", (req, res) => {
-  res.send("API Running");
+  res.header("Access-Control-Allow-Origin", "허용하고자 하는 도메인");
+  res.send(data);
 });
 
 mongoose.connect(process.env.MONGO, () => {
