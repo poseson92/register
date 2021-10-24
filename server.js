b9a38a74d4a0a14aacc7b6deea24767d8dbbc80c
Bong <poseson92@naver.com>
2021-10-25 03:55:54 +0900

    Update7

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(express.json({ extended: false }));
 app.use("/api/register", require("./routes/api/register"));
 
 app.get("/", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "허용하고자 하는 도메인");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://still-basin-28888.herokuapp.com/"
+  );
   res.send(data);
 });
 
