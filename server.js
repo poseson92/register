fa06a6180e16e825ed588a7ad418c2184d8664b1
Bong <poseson92@naver.com>
2021-10-25 04:22:27 +0900

    Update9

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const { use } = require("./routes/api/register");
 
 const app = express();
+
+app.use(cors());
 
 dotenv.config({ path: "./config.env" });
 
